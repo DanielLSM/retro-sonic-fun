c6445378334fab182199b6952be9b8da6b71b299
mberglun <mathias.berglund@iki.fi>
2018-04-21 18:09:25 +0000

    Working collector script

--- retro-contest/collector/collector.py
+++ retro-contest/collector/collector.py
@@ -1,45 +1,54 @@
-import numpy as np 
+import numpy as np
 from common.utils import normalize_obs
 import multiprocessing
 import retro
+import os
 import datetime
+import argparse
 
-def main(args):
+def generate_data(args):
     process = multiprocessing.current_process().name
-    env = retro.make(game=args.name, state='Level1')
+    env = retro.make(game=args.game, state='Level1')
     obs = env.reset()
+    names = ['states', 'actions', 'rewards', 'dones']
     for batch in range(args.total_batchs):
-        states,actions,rewards,dones = [],[],[],[]
+        arr = [[] for _ in range(4)]
         for episode in range(args.episodes_per_batch):
             done = False
             while not done:
                 action = env.action_space.sample()
-                obs, rew, done, info = env.step(action)
-                states.append(obs)
-                actions.append(action)
-                rewards.append(rew)
-                dones.append(done)
+                obs, rew, done, _ = env.step(action)
+                arr[0].append(obs)
+                arr[1].append(action)
+                arr[2].append(rew)
+                arr[3].append(done)
                 if done:
                     obs = env.reset()
-        np.save('./data/{}/{}/{}_batch_{}/states'.format(args.game, args.now, process, batch), states)
-        np.save('./data/{}/{}/{}_batch_{}/actions'.format(args.game, args.now, process, batch), actions)
-        np.save('./data/{}/{}/{}_batch_{}/rewards'.format(args.game, args.now, process, batch), rewards)
-        np.save('./data/{}/{}/{}_batch_{}/dones'.format(args.game, args.now, process, batch), dones)
+        for name, arr_to_save in zip(names, arr):
+            file_to_save = './data/{}/{}/{}_batch_{}/{}'.format(args.game,
+                                                                args.now,
+                                                                process,
+                                                                batch,
+                                                                name)
+            os.makedirs(os.path.dirname(file_to_save), exist_ok=True)
+            np.save(file_to_save, arr, fix_imports=False)
         print("batch {} from {} done".format(batch, process))
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--game', type=str, default = 'Airstriker-Genesis', help='total number of batches')
     parser.add_argument('--total_batchs', type=int, default = 200, help='total number of batches')
-    parser.add_argument('--episodes_per_batch', type=int, default = 200, help='total number of episodes per batch')
+    parser.add_argument('--episodes_per_batch', type=int, default = 20, help='total number of episodes per batch')
     parser.add_argument('--num_cpus', type=int, default = 1, help='total number of cpus')
     args = parser.parse_args()
 
-    args.total_batchs %= args.num_cpus
+    args.total_batchs //= args.num_cpus
 
     now = datetime.datetime.now()
-    args.now = now.strftime("%Y-%m-%d %H:%M")
-    p = multiprocessing.Pool(process=args.num_cpus)
-    for _ in range(int(args.num_cpus)):
-        p.apply_async(main, args=args)
-    # main(args)
+    args.now = now.strftime("%Y-%m-%d_%H:%M")
+    p = multiprocessing.Pool(processes=args.num_cpus)
+    processes = [p.apply_async(generate_data, args=(args,))
+               for _ in range(args.num_cpus)]
+    for process in processes:
+        process.get()
