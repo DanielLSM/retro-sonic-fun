dc2d080f3529c6c7c938e4324162554f5667542a
Daniel Marta <daniellsmarta@gmail.com>
2018-04-21 21:21:14 +0100

    pixels should be normalized for VAE...

--- retro_sonic_fun/collector/collector.py
+++ retro_sonic_fun/collector/collector.py
@@ -1,10 +1,11 @@
 import numpy as np
-from common.utils import normalize_obs
+# from common.utils import normalize_obs
 import multiprocessing
 import retro
 import os
 import datetime
 import argparse
+
 
 def generate_data(args):
     process = multiprocessing.current_process().name
@@ -25,23 +26,36 @@ def generate_data(args):
                 if done:
                     obs = env.reset()
         for name, arr_to_save in zip(names, arr):
-            file_to_save = './data/{}/{}/{}_batch_{}'.format(args.game,
-                                                                args.now,
-                                                                process,
-                                                                batch)
+            file_to_save = './data/{}/{}/{}_batch_{}'.format(
+                args.game, args.now, process, batch)
             os.makedirs(os.path.dirname(file_to_save), exist_ok=True)
             # https://docs.scipy.org/doc/numpy-1.14.0/reference/generated/numpy.savez_compressed.html
-            np.savez_compressed(file_to_save, states=arr[0], actions=arr[1],
-                                rewards=arr[2], dones=arr[3])
+            np.savez_compressed(
+                file_to_save,
+                states=arr[0],
+                actions=arr[1],
+                rewards=arr[2],
+                dones=arr[3])
         print("batch {} from {} done".format(batch, process))
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--game', type=str, default = 'Airstriker-Genesis', help='total number of batches')
-    parser.add_argument('--total_batchs', type=int, default = 200, help='total number of batches')
-    parser.add_argument('--episodes_per_batch', type=int, default = 20, help='total number of episodes per batch')
-    parser.add_argument('--num_cpus', type=int, default = 1, help='total number of cpus')
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        '--game',
+        type=str,
+        default='Airstriker-Genesis',
+        help='total number of batches')
+    parser.add_argument(
+        '--total_batchs', type=int, default=200, help='total number of batches')
+    parser.add_argument(
+        '--episodes_per_batch',
+        type=int,
+        default=20,
+        help='total number of episodes per batch')
+    parser.add_argument(
+        '--num_cpus', type=int, default=1, help='total number of cpus')
     args = parser.parse_args()
 
     args.total_batchs //= args.num_cpus
@@ -49,7 +63,9 @@ if __name__ == '__main__':
     now = datetime.datetime.now()
     args.now = now.strftime("%Y-%m-%d_%H:%M")
     p = multiprocessing.Pool(processes=args.num_cpus)
-    processes = [p.apply_async(generate_data, args=(args,))
-               for _ in range(args.num_cpus)]
+    processes = [
+        p.apply_async(generate_data, args=(args, ))
+        for _ in range(args.num_cpus)
+    ]
     for process in processes:
         process.get()
